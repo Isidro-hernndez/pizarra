6a276d9e12d3222aa1112416b60af3a136b1ff06
Abraham Toriz <awonderfulcode@gmail.com>
2019-04-06 21:28:38 -0400

    la documentación de pizarra

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,36 @@
+/*!
+Pizarra pretende ser una herramienta para hacer presentaciones en vivo, por que
+creo que tener un pizarrón y gises es lo mejor para explicar cosas.
+
+La mejor forma de utilizar pizarra es teniendo una tabla de dibujo.
+
+# Atajos de teclado
+
+Pizarra por el momento se puede controlar con algunos atajos de teclado que
+permiten cambiar la herramienta en uso, colores y otras configuraciones.
+
+## Herramientas de dibujo
+
+* `Ctrl+R` Rectángulo
+* `Ctrl+L` Línea
+
+## Colores
+
+* `Alt+G` Verde
+* `Alt+R` Rojo
+* `Alt+B` Azul
+* `Alt+Y` Amarillo
+* `Alt+O` Naranja
+* `Alt+W` Blanco
+
+# Archivo de salida
+
+Pizarra guarda actualmente un archivo svg en el directorio en que es ejecutada
+con el dibujo hecho durante la sesión. El nombre del archivo es único y lleva
+la fecha en hora local en que se hizo.
+
+*/
+
 pub mod color;
 pub mod poly;
 pub mod storage;
